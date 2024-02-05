1ced5c2dfe5ff0086996e71b404dd82f54f530c1
randifus <159055125+randifus@users.noreply.github.com>
2024-02-05 09:30:04 -0800

    Add files via upload

---
+++ js/assets-index.c6e042ae.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>u dumb asf 💀💀</title>
+    <link rel="icon" type="image/png" href="famicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Consolas&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background: linear-gradient(to right, red, orange);
+            color: #fff;
+            font-family: 'Consolas', sans-serif;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            margin: 0;
+        }
+        
+        h1 {
+            font-size: 36px;
+            margin-top: 50px;
+        }
+        
+        p {
+            font-size: 18px;
+            margin: 25px 0;
+        }
+        
+        footer {
+            margin-top: 50px;
+            font-size: 16px;
+        }
+        
+        @media screen and (max-width: 600px) {
+            h1 {
+                font-size: 24px;
+                margin-top: 25px;
+            }
+            p {
+                font-size: 20px;
+                margin: 15px 0;
+            }
+        }
+        
+        a {
+            color: white
+        }
+        
+        a:hover {
+            color: gray
+        }
+        
+        a:active {
+            color: black
+        }
+    </style>
+</head>
+
+<body>
+    <h1>error 404, page not found. the page you're looking for cannot be found. if you entered the URL manually, check the URL and try again, otherwise, the page was probably moved or deleted.</h1>
+</body>
+
+</html>
